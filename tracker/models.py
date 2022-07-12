587fe1ce4839a8b314ef44b22f14247f05809ba0
ryanfrenia <ryanfrenia@gmail.com>
2022-07-12 14:44:06 -0400

    updated settings

--- tracker/models.py
+++ tracker/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
 class User(AbstractUser):
     pass
     #user_name = models.CharField(max_length=255, null=True, blank=True)
